5aaf618777cdcb1ff7f2d5c268f06dd162039b9a
aobenauf <obenaufalex@gmail.com>
2018-05-08 20:33:56 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-consumer_key = 'jojPVykSJGVhS5slJ0XyXivHu'
-consumer_secret ='ZW5f2kayCsiiKalU3HIC3KFX0ST0DCm1yPIZdpKaqlRlOSuI7G'
-access_token = '426390930-7XETBHfeBDnsErvWEK7B7DmpSFeKDOH5srAzfEyx'
-access_token_secret = 'gW06K30hZwNHRi12W3n758Fq6BNG7xQzNxmGUdr84iPLS'
-
-secret_key = 'i+8khqy*9pzil%u=rbsprmcp!p@sr)ob11t39x$zd0rq$n6q64'
